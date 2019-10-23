ee55480bd4929c57a8f92e57c39316d0e09cf6b8
Andrea Desiderio <andreadesideriopro@gmail.com>
2019-10-22 18:03:31 -0700

    removed spinners from input=number for firefox

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,7 +54,7 @@ function displayErrorMessage(message){
 
 function camelize(str){
     return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, function(match, index) {
-      if (+match === 0) return ""; // or if (/\s+/.test(match)) for white spaces
+      if (+match === 0) return "";
       return index == 0 ? match.toLowerCase() : match.toUpperCase();
     });
   }
